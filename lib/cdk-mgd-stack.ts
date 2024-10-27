0df199e4354ee3a2d960d67715013704551d7423
sciberbee <yby0907200@gmail.com>
2024-10-28 07:08:23 +0900

    Part 1 Done

--- lib/cdk-mgd-stack.ts
+++ lib/cdk-mgd-stack.ts
@@ -1,16 +1,26 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3n from 'aws-cdk-lib/aws-s3-notifications';
 
 export class CdkMgdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucket = new s3.Bucket(this, 'CdkMgdBucket', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // the bucket will be deleted when the stack is deleted
+      autoDeleteObjects: true, // force all objects to get deleted during bucket deletion
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkMgdQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const queue = new sqs.Queue(this, 'CdkMgdQueue', {
+      visibilityTimeout: cdk.Duration.seconds(300),
+    });
+
+    bucket.addEventNotification(
+      s3.EventType.OBJECT_CREATED_PUT,
+      new s3n.SqsDestination(queue),
+      { prefix: 'input/' } // input/ prefix에만 이벤트 발생
+    );
   }
 }
